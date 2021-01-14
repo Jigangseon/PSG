0abc888653739e0b317ffaf2ba6a9b7a54b489ff
P-SG <psgxxx@naver.com>
2021-01-14 18:12:24 +0900

    프로젝트 시작

--- app/src/main/java/com/example/psg/MainActivity.java
+++ app/src/main/java/com/example/psg/MainActivity.java
@@ -11,9 +11,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //김강
-    //개 모 대
-    //정신나갈거가태
-    //배고파!
-    //테스트
 }
