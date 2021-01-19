fc8e03f24437a092f53d94350d7dad818968e51a
seonrae <ksr5445@gmail.com>
2021-01-19 20:45:54 +0900

    선래 커밋

---
+++ app/src/main/java/com/jigangseon/psg/SignUpMain.java
@@ -0,0 +1,28 @@
+package com.jigangseon.psg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_main);
+    }
+
+    public void signIn(View view){
+//        Intent intent = new Intent(getApplicationContext(), SignIn.class);
+//        startActivity(intent);
+    }
+    public void signUp(View view){
+//        Intent intent = new Intent(getApplicationContext(), SignUp.class);
+//        startActivity(intent);
+    }
+    public void guestIn(View view){
+//        Intent intent = new Intent(getApplicationContext(), GuestIn.class);
+//        startActivity(intent);
+    }
+}
